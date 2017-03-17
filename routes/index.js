7572eb8041e06016d8ec6eedd0fef73e076d6811
cyril lopez <cyril.lpz@gmail.com>
2017-03-17 11:06:48 +0100

    error

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 module.exports = (server) => {
     server.use('/users', require('./users')(server));
-    server.use('/Tasks', require('./Tasks')(server));
+    // server.use('/Tasks', require('./Tasks')(server));
     server.use('/auth', require('./auth')(server));
+    server.use('/projects', require('./projects')(server))
 };
