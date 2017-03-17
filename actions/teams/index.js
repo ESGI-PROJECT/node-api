070bbf45cefcd2349abcda86eef9c17081a4b6dd
patak <pataky@hotmail.fr>
2017-03-17 12:12:20 +0100

    [feat] - add user to team

--- actions/teams/index.js
+++ actions/teams/index.js
@@ -1,5 +1,6 @@
 module.exports = (server) => {
   const Team = server.models.Team;
+  const User = server.models.Team;
 
   return {
       list,
@@ -24,5 +25,31 @@ module.exports = (server) => {
           .catch(res.error);
   }
 
-  
+  function addUser(req,res,next) {
+
+
+    Team.findById(req.team.id)
+        .then(server.utils.ensureOne)
+        .catch(server.utils.reject(404, 'Team.not.found'));
+        .then(addUserToTeam)
+        .then(res.commit)
+        .catch(res.error);
+
+    function addUserToTeam (data) {
+      User.findById(req.user.id)
+          .then(server.utils.ensureOne)
+          .catch(server.utils.reject(404, 'User.not.found'));
+          .then(addToTeam)
+          .then(returnTeam)
+
+      function addToTeam(Team) {
+        Team.users.push(User._id)
+        Team.save()
+      }
+
+      function returnTeam() {
+        return Team;
+      }
+    }
+  }
 };
